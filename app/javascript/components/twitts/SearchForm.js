48e57ae8ce95dfbccf57b1e938b3fc75fa7bff6f
Mike <m.kniazevich@coaxsoft.com>
2018-08-29 14:29:46 +0300

    Add react-tagsinput

--- app/javascript/components/twitts/SearchForm.js
+++ app/javascript/components/twitts/SearchForm.js
@@ -1,19 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css'
+
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
     this.token = props.csrf_token;
+    this.state = {tags: []};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(tags) {
+    this.setState({tags})
   }
 
   render () {
     return (
-      <form action="/twitts/index">
+      <form action="/v1/twitts">
         <input type="hidden" name="authenticity_token" value={this.token} readOnly={true} />
-        <div className="form-group">
-          <label htmlFor="tags">Input tags, please:</label>
-          <input type="text" className="form-control" name="tags" />
-        </div>
+        <TagsInput value={this.state.tags} onChange={this.handleChange} addKeys={[9, 13, 32]}/>
         <button type="submit" className="btn btn-primary">
           Go
         </button>
